95022fce486fcf12503d70e7ae3b2a1405e1361c
hansolChoi29 <ggksthf29@gmail.com>
2024-12-18 00:39:08 +0900

    feat:route error

--- src/app/api/rotation/route.ts
+++ src/app/api/rotation/route.ts
@@ -2,29 +2,27 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    // Riot API 호출
     const response = await fetch(
       "https://kr.api.riotgames.com/lol/platform/v3/champion-rotations",
       {
         headers: {
-          "X-Riot-Token": process.env.RIOT_API_KEY || "", // API 키 환경변수 사용
+          "X-Riot-Token": process.env.RIOT_API_KEY || "",
         },
       }
     );
 
-    // API 응답 확인
     if (!response.ok) {
-      console.error("Riot API 호출 실패:", response.status);
+      console.error("API 요청 실패:", response.statusText);
       return NextResponse.json(
-        { error: "Failed to fetch champion rotation data" },
+        { error: "API 호출 실패" },
         { status: response.status }
       );
     }
 
     const data = await response.json();
-    return NextResponse.json(data); // 성공적으로 데이터 반환
+    return NextResponse.json(data);
   } catch (error) {
-    console.error("API Route 내부 오류:", error);
+    console.error("서버 내부 오류:", error);
     return NextResponse.json(
       { error: "Internal Server Error" },
       { status: 500 }
