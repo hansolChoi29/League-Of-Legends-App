539083685d36d438e86808a2a9a6ca52cf5ab3a6
hansolChoi29 <ggksthf29@gmail.com>
2024-12-18 04:07:22 +0900

    feat:first 1 force-dynamic

--- src/app/champions/[id]/page.tsx
+++ src/app/champions/[id]/page.tsx
@@ -5,41 +5,24 @@ import { notFound } from "next/navigation";
 // SSR 강제: 항상 최신 데이터를 가져옵니다.
 // export const dynamic = "force-dynamic";
 // 동적렌더링 제어
-export async function generateStaticParams() {
-  const response = await fetch(
-    "https://ddragon.leagueoflegends.com/cdn/14.24.1/data/ko_KR/champion.json"
-  );
-  const data = await response.json();
-  console.log("Response Status:", response.status);
-
-  return Object.keys(data.data).map((id) => ({ id }));
-}
-
-// ISR (Incremental Static Regeneration) 활용
-export const revalidate = 60; // 60초마다 새 데이터 확인
-
-export const dynamicParams = true; // 미리 생성되지 않은 경로도 SSR로 처리
+export const dynamic = "force-dynamic"; // SSR 강제
 
 export default async function ChampionDetailPage({
   params,
 }: {
   params: { id: string };
 }) {
-  console.log("Params on Vercel:", params);
+  console.log("Params Received:", params);
 
+  // Riot API에서 데이터 가져오기
   const response = await fetch(
     `https://ddragon.leagueoflegends.com/cdn/14.24.1/data/ko_KR/champion/${params.id}.json`,
-    {
-      headers: {
-        "X-Riot-Token": process.env.RIOT_API_KEY || "",
-      },
-      cache: "no-store",
-    }
+    { cache: "no-store" } // 항상 최신 데이터
   );
 
   if (!response.ok) {
     console.error("Failed to fetch data:", response.status);
-    notFound(); // 404 처리
+    notFound(); // 404 페이지 반환
   }
 
   const data = await response.json();
